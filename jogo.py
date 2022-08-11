0a0a9cb2ef15745d6d34eb9cb2fe955ed72e6953
DOUGLAS SILVA OLIVEIRA <102192988+DGsilva2@users.noreply.github.com>
2022-08-11 11:15:24 -0300

    Add files via upload

---
+++ jogo.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+janela =Tk()
+janela.title('calculadora')
+janela.geometry('235x318')
+frame_tela = frame(janela, widht=235 , height=50)
+janela.mainloop()
